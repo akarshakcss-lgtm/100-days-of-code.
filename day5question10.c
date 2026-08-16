14ecbb2c234716b98cfee46bc1c934f6c697b908
Akarshak Srivastava <akarshak.css@gmail.com>
2026-08-16 21:46:14 +0530

    Add files via upload

--- day5question10.c
+++ day5question10.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
 int main (){
-    int total_seconds;
-    printf("enter total_seconds\n");
+    int total_seconds ;
+    printf("enter total seconds\n");
     scanf("%d", &total_seconds);
-    int hours = total_seconds/3600;
-    int minutes = (total_seconds%3600)/60;
-    int seconds = (total_seconds%60);
-    printf("afte converting seconds hours:minutes:seconds format \n= %d%d%d", hours,minutes,seconds);
-    return 0; 
+    int hours = total_seconds /3600 ;
+    int minutes = (total_seconds % 3600)/60 ;
+    int seconds = total_seconds % 60 ;
+ printf("seconds convert into hours:minutes:seconds is = \n%d%d%d", hours, minutes ,seconds);
+return 0 ;
 }
--- day5question9.c
+++ day5question9.c
@@ -1,20 +1,19 @@
-#include<stdio.h>
-#include<math.h>
+#include <stdio.h>
+#include <math.h>
 int main() {
-    float P;
+    float P, R, T;
     printf("enter principal amount\n");
-    scanf("%f" , &P);
-    float R ;
+    scanf("%f", &P);
     printf("enter rate of interest\n");
     scanf("%f", &R);
-    float T ;
-    printf("enter time\n");
-    scanf("%f" , &T);
-   float simple_interest = P*R*T/100 ;
-   float amount = P+simple_interest;
-   float compound_interest = P*pow(1+R/100,T)-P;
-    printf("simple interest is : = %f\n",simple_interest);
-    printf("amount =%f\n",amount);
-    printf("compound interest is : %.2f\n",compound_interest);
-    return 0 ;
+    printf("enter time in years\n");
+    scanf("%f", &T);
+    float simple_interest = (P * R * T) / 100.0;
+    printf("simple interest is %.2f\n", simple_interest);
+    float total_amount = P + simple_interest;
+    printf("total amount is %.2f\n", total_amount);
+    float compound_interest = P * (pow(1.0 + R / 100.0, T)) - P;
+    printf("compound_interest is %.2f\n", compound_interest);
+    return 0;
 }
+
