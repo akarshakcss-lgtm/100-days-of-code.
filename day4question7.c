b9129040408026d6bcd44de7986a7ae3bc2843fa
Akarshak Srivastava <akarshak.css@gmail.com>
2026-08-18 23:57:57 +0530

    Add files via upload

--- day4question7.c
+++ day4question7.c
@@ -1,12 +1,28 @@
-#include<stdio.h>
-int main (){
-    int n1;
-    printf("enter first number\n");
-    scanf("%d", &n1);
-    int n2;
-    printf("enter second number\n");
-    scanf("%d", &n2);
-    n1,n2 = n2,n1 ;
-    printf("afters swap %d %d", n1,n2);
+//Q7: Write a program to swap two numbers without using a third variable.
+/*
+Sample Test Cases:
+Input 1:
+10 20
+Output 1:
+After swap: 20 10
+
+Input 2:
+7 14
+Output 2:
+After swap: 14 7
+
+*/
+#include <stdio.h>
+int main()
+{
+    int a, b;
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+    a = a + b;
+    b = a - b;
+    a = a - b;
+    printf("After swapping:\n");
+    printf("a = %d\n", a);
+    printf("b = %d\n", b);
     return 0;
 }
